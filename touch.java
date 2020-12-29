39375d488ecbc85137c252dc1b8e7b5aed97b920
agasan371119 <yna77351unichi@icloud.com>
2020-12-29 19:42:17 +0900

    copy練習2

--- touch.java
+++ touch.java
@@ -3,14 +3,17 @@ import java.util.Random;
 
 public class touch {
   public static void main(String[] args) {
-    int answer = new Random().nextInt(10) + 1;
+    int answer = new Random().nextInt(20) + 1;
     int count = 0;
     
+
+    //繰り返し
     while(true) {
       System.out.print("Your guess? ");
       int guess = new Scanner(System.in).nextInt();
       count++;
-
+      
+      //大小判定
       if(answer == guess) {
         System.out.println("Bingo! It took " + count + " gusses!");
         break;
