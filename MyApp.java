0859310263559fccc5605e902c301557580d6ceb
agasan371119 <yna77351unichi@icloud.com>
2021-01-08 10:31:21 +0900

    練習13

--- MyApp.java
+++ MyApp.java
@@ -1,23 +1,8 @@
 public class MyApp {
   public static void main(String[] args) {
+    int score = 45;
 
-    //演算子
-    //+-*/%
-    //++ --
-    int i ;
-    i = 10 / 3;
-    System.out.println(i);
-    i = 10 % 3;
-    System.out.println(i);
-
-    int x = 5;
-    x++;
-    System.out.println(x);
-    x--;
-    System.out.println(x);
-    x *= 3;
-    System.out.println(x);
-
-
-  }
+    String msg = score > 80 ? "Great!!" : "so.. so..!";
+    System.out.println(msg);  
+  } 
 }
