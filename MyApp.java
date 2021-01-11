08922d5fa79a2f7425cc93ec4927d52c9924865c
agasan371119 <yna77351unichi@icloud.com>
2021-01-11 11:39:06 +0900

    練習16

--- MyApp.java
+++ MyApp.java
@@ -1,18 +1,18 @@
 public class MyApp {
   public static void main(String[] args) {
-    String signal = "green";
+    int[] salse = {100, 200, 300};
 
-    switch(signal) {
-      case "red":
-        System.out.println("stop!");
-        break;
-      case "blue":
-      case "green":
-        System.out.println("go!!!!!");
-        break;
-      case "yellow":
-        System.out.println("cautaion!!");
-        break;
+    //通常for文
+    for(int i = 0; i < salse.length; i++) {
+      System.out.println(salse[i]);
     }
+
+    System.out.println("---");
+
+    //拡張for文
+    for(int sale : salse) {
+      System.out.println(sale);
+    }
+
   } 
 }
