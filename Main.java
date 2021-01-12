9ce8838a8ed10a3d8d3bc854b8de08e003f4771b
agasan371119 <yna77351unichi@icloud.com>
2021-01-12 21:57:27 +0900

    copy練習15

---
+++ Main.java
@@ -0,0 +1,10 @@
+//引数と戻り値
+public class Main {
+  public static void main(String[] args) {
+    sayHello("bob");
+  }
+
+  public static void sayHello(String name) {
+    System.out.println("hello " + name);
+  }
+}
