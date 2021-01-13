c844025849dc2a3babed6a482fd23ab36867f026
agasan371119 <yna77351unichi@icloud.com>
2021-01-13 18:26:28 +0900

    copy練習8

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
-//引数と戻り値
+//戻り値
 public class Main {
   public static void main(String[] args) {
-    sayHello("Bob");
-    sayBay("Kento");
-    sayHello("Bob");
-    sayBay("Kento");
+      int sum1 = sum(300, 500);
+      System.out.println(sum1);
+      int sum2 = product(500, 700);
+      System.out.println(sum2);
   }
 
-  public static void sayHello(String name) {
-    System.out.println("hello " + name);
+  public static int sum(int a, int b) {
+    return a + b;
   }
 
-  public static void sayBay(String name) {
-    System.out.println("bay " + name);
+  public static int product(int x, int y) {
+    return x * y;
   }
-
 }
