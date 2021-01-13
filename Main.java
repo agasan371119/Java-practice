95b82f59a537974fb50c694a4bdc81ce490bbd38
agasan371119 <yna77351unichi@icloud.com>
2021-01-13 17:33:07 +0900

    copy練習7

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 //引数と戻り値
 public class Main {
   public static void main(String[] args) {
-    sayHello("bob");
+    sayHello("Bob");
+    sayBay("Kento");
+    sayHello("Bob");
+    sayBay("Kento");
   }
 
   public static void sayHello(String name) {
     System.out.println("hello " + name);
   }
+
+  public static void sayBay(String name) {
+    System.out.println("bay " + name);
+  }
+
 }
