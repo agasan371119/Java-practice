7145b4591df42ebf33fca375ea61213728b8db2a
agasan371119 <yna77351unichi@icloud.com>
2021-01-16 10:58:31 +0900

    メソッド練習

--- MainMethod.java
+++ MainMethod.java
@@ -1,11 +1,27 @@
 public class MainMethod {
   public static void main(String[] args) {
-
+    monster();
+    person();
   }
 
-  
 
-  public static void attack() {
+  //モンスターの発生
+  public static void monster() {
+    String[] enemies = {"スライム", "ドラゴン", "魔王"};
 
+    int rand = (int)(Math.random() * 3 + 0);
+
+    System.out.println(enemies[rand] + "が、現れた!!");
+    System.out.println(" ");
+  }
+
+  //仲間の準備
+  public static void person() {
+    String[] parties = {"勇者", "戦士", "魔法使い", "武道家", "遊び人"};
+
+    for(int i = 0; i < parties.length - 1; i++) {
+      System.out.println(parties[i] + "は、身構えた!!");
+    }
+    System.out.println(parties[parties.length - 1] + "は、遊んでいる");
   }
 }
