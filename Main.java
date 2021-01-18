5852d53c8269c1b6a01efa2976e5800372f2d011
agasan371119 <yna77351unichi@icloud.com>
2021-01-18 17:37:35 +0900

    メソッド配列1

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-    int[] array = {1, 2, 3};
+    String[] enemy = {"勇者", "戦士", "武道家", "遊び人"};
 
-    printArray(array);
+    printArray(enemy);
   }
-  public static void pringArray(int[] array) {
-    for(int element : array) {
-      System.out.println(element);
+  public static void printArray(String[] enemy) {
+    for(int i = 0; i < enemy.length - 1; i++) {
+      System.out.println(enemy[i] + "武器を構えた");
     }
+    System.out.println(enemy[3] + "は遊んでいる");
   }
 }
