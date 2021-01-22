9a5953338bab828230e75a06bd9d2e2adbad5201
agasan371119 <yna77351unichi@icloud.com>
2021-01-23 06:23:37 +0900

    練習3

--- Main.java
+++ Main.java
@@ -3,11 +3,16 @@ public class Main {
     String[] enemy = {"勇者", "戦士", "武道家", "遊び人"};
 
     printArray(enemy);
+    attack();
   }
   public static void printArray(String[] enemy) {
     for(int i = 0; i < enemy.length - 1; i++) {
       System.out.println(enemy[i] + "武器を構えた");
     }
     System.out.println(enemy[3] + "は遊んでいる");
+  }
+
+  public static void attack() {
+    System.out.println("剣で攻撃");
   }
 }
