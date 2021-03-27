d326ea3889005182b867a985eb84c88afae0731a
agasan371119 <yna77351unichi@icloud.com>
2021-03-28 00:16:45 +0900

    練習64

--- Main.java
+++ Main.java
@@ -1,23 +1,20 @@
+import java.util.*;
+import java.util.Arrays;
+
+
 public class Main {
-  public static void main(String[] args) {
-      User tom = new User();
-
-      System.out.println(jon.name);
-      jon.sayHi();
-
-      sayHello();
-
-  }
-
-  public static void sayHello() {
-    System.out.println("hey siri");
-  }
-}
-
-class User {
-  String name = "11111";
-
-  void sayHi() {
-    System.out.println("愛知 静岡 赤坂 福岡 尾張 名古屋 愛知 能登!");
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        
+        int[] num = new int[count];
+        
+        for(int i = 0; i < count; i++) {
+            num[i] = sc.nextInt();
+        }
+        //配列を昇順に並び替え
+        Arrays.sort(num);
+        //配列の一番大きい数字を出力
+        System.out.println(num[count - 1]);
+    }
 }
