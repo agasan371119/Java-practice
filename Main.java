1e026baf7479e364492604996a43f23f44002335
agasan371119 <yna77351unichi@icloud.com>
2021-03-29 19:25:51 +0900

    復習

--- Main.java
+++ Main.java
@@ -1,20 +1,24 @@
 import java.util.*;
-import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //カウント
         int count = sc.nextInt();
-        
-        int[] num = new int[count];
-        
+        //数字の初期化
         for(int i = 0; i < count; i++) {
-            num[i] = sc.nextInt();
+          int num = sc.nextInt();
+
+          if(num == 2) {
+            System.out.println("Same");
+          } 
+
+          if(num % 2 == 1){
+            System.out.println("Odd");
+          } else {
+            System.out.println("Even");
+          }
         }
-        //配列を昇順に並び替え
-        Arrays.sort(num);
-        //配列の一番大きい数字を出力
-        System.out.println(num[count - 1]);
     }
 }
