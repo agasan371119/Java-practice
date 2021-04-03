3adba3411eaefb9bbd03db6fd15dbbd4d624e8d8
agasan371119 <yna77351unichi@icloud.com>
2021-04-04 00:33:41 +0900

    復習6

--- Main.java
+++ Main.java
@@ -3,20 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //カウント
-        int count = sc.nextInt();
-        //数字の初期化
-        for(int i = 0; i < count; i++) {
-          int num = sc.nextInt();
-
-          if(num % 4 == 0){
-            System.out.println("Even");
-          } else if(num % 4 == 0 && num % 2 == 0){
-            System.out.println("Same");
-          } else {
-            System.out.println("Odd");
-          }
-        }
+      System.out.println("hello");
     }
 }
