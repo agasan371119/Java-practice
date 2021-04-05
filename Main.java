9b48aa82e94b06b72a48f8a06d278688cd3c03bc
agasan371119 <yna77351unichi@icloud.com>
2021-04-05 15:37:47 +0900

    復習8

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
-      System.out.println("hello paiza");
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      String str = sc.next();
+
+      int sum  = a + b + c;
+      System.out.println(sum + " " + str);
+      
     }
 }
