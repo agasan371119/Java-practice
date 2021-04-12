fe716daf9432c76a3e9cba6257e486a872cfe81a
agasan371119 <yna77351unichi@icloud.com>
2021-04-12 17:09:37 +0900

    復習15

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+      System.out.println("メソッドを呼び出します");
+      hello();
+      System.out.println("メソッドの呼び出しが終わりました");
+    }
 
-      //繰り返しの数
-      int num = sc.nextInt();
-
-      System.out.println(num - 1);
+    public static void hello() {
+      System.out.println("湊さんこんにちは");
     }
 }
