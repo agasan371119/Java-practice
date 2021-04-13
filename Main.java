819a0e8a2b773a8f041efba2231834a864dd3ed2
agasan371119 <yna77351unichi@icloud.com>
2021-04-13 21:42:04 +0900

    復習FizzBuzz1

--- Main.java
+++ Main.java
@@ -2,12 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-      hello();
-    }
-
-    public static void hello() {
       Scanner sc = new Scanner(System.in);
-      String str = sc.nextLine();
-      System.out.println(str);
+      //数の指定
+      int count = sc.nextInt();
+
+      //空白
+      System.out.println("---------------");
+
+      //繰り返し
+      String output;
+      for(int i = 1; i <= count; i++) {
+          if(i % 3 == 0 && i % 5 == 0) {
+            output = i + ": FizzBuzz";
+          } else if(i % 3 == 0) {
+            output = i + ": Fizz";
+          } else if(i % 5 == 0) {
+            output = i + ": Buzz";
+          } else {
+            output = String.valueOf(i);
+          }
+          System.out.println(output);
+      }
     }
 }
