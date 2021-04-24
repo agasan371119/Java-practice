4bb9f57a67153e3f03d7be0fd4c166e524e3d4d2
agasan371119 <yna77351unichi@icloud.com>
2021-04-24 21:03:18 +0900

    FizzBuzz復習13

--- Main.java
+++ Main.java
@@ -3,29 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      //数の指定
-      int count = sc.nextInt();
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
 
-      //空白
-      System.out.print(":   ");
-      System.out.println("- - - - - - - - - - - - - - - - - - - - -");
-      System.out.println("ここから入力");
+      int s = a * a + b * b;
+      int u = c * c;
 
-      //繰り返し
-      String output;
-      for(int i = 1; i <= count; i++) {
-        output = "";
-
-        if(i % 3 == 0) {
-          output = "Fizz";
-        }
-
-        if(i % 5 == 0) {
-          output = output + "Buzz";
-        }
-
-        output = String.valueOf(i) + " : " + output;
-        System.out.println(output);
+      if(s < u) {
+        System.out.println("Yes");
+      } else {
+        System.out.println("No");
       }
+      
     }
 }
