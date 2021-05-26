7c3a17b1c53e3f7c6b19c22ef53b7c6ddc2ff025
agasan371119 <yna77351unichi@icloud.com>
2021-05-26 13:15:31 +0900

    練習32

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
       if(s <= u) {
         System.out.println("zimen - kamieeee");
       } else {
-        System.out.println("いや勉強");
+        System.out.println("いや勉強しろよ");
       }
     }
 }
