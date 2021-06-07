bf9e816e089e7056f1d8001507804af52f95ce11
agasan371119 <yna77351unichi@icloud.com>
2021-06-07 18:43:28 +0900

    練習44

--- Main.java
+++ Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
     
-      int num = sc.nextInt();
+      int s = sc.nextInt();
+      int y = sc.nextInt();
 
       for(int i = 0; i < num; i++) {
         System.out.println("YES!!!!!!!!??????");
       } else {
-        System.out.println("NO!!!!!!!!???%%%%%%%");
+        System.out.println("NO!!!!!!!!%%%");
       }
     }
 }
