71b66f33426c74c03c83ca63c9fa2fe98d6a200c
agasan371119 <yna77351unichi@icloud.com>
2021-06-13 00:21:38 +0900

    練習50

--- Main.java
+++ Main.java
@@ -1,15 +1,12 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      
-      System.out.print("数字を入力してください : ");
-      int count = sc.nextInt();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int count = sc.nextInt();
 
-
-      for(int i = 1; i < count; i++) {
-        System.out.println(i);
+    for(int i = 0; i < count; i++) {
+      System.out.println(i);
     }
   }
 }
