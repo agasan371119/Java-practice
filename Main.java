785030655f968aa2216d73eb58e3998632365f24
agasan371119 <yna77351unichi@icloud.com>
2021-06-15 19:25:36 +0900

    練習1

--- Main.java
+++ Main.java
@@ -4,10 +4,10 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     double num = sc.nextInt();
-    double num1 = sc.nextInt();
+    double num2 = sc.nextInt();
 
-    double sum = num1 / 100;
-    double sum1 = sum1 * num2;
+    double sum = num2 / 100;
+    double sum2 = sum * num2;
 
     System.out.println(sum2);
 
