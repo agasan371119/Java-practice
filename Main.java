fd5d2baf6395e6b99a4a3960e83f14739cc239e7
agasan371119 <yna77351unichi@icloud.com>
2021-06-16 17:03:58 +0900

    練習2

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    double num = sc.nextInt();
-    double num2 = sc.nextInt();
+    int num = sc.nextInt();
+    int num2 = sc.nextInt();
 
-    double sum = num2 / 100;
-    double sum2 = sum * num2;
+    int sum = num2 / 100;
+    int sum2 = sum * num2;
 
     System.out.println(sum2);
 
