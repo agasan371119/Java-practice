94808fdc9862f1406dce817e62b79d863da2965f
agasan371119 <yna77351unichi@icloud.com>
2021-06-19 17:28:02 +0900

    練習5

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String s = sc.nextLine();
+    String num = sc.nextLine();
 
-    System.out.println(s);
+    System.out.println(num);
   }
 }
