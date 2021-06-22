f5645a9b914145134cf90e62878b58579797fe1a
agasan371119 <yna77351unichi@icloud.com>
2021-06-22 21:06:29 +0900

    練習1

--- MyApp.java
+++ MyApp.java
@@ -1,18 +1,13 @@
 public class MyApp {
   public static void main(String[] args) {
-    int[] salse = {100, 200, 300};
+    Player player1 = new Player();
+    player1.walk();
+  }
 
-    //通常for文
-    for(int i = 0; i < salse.length; i++) {
-      System.out.println(salse[i]);
-    }
+}
 
-    System.out.println("---");
-
-    //拡張for文
-    for(int sale : salse) {
-      System.out.println(sale);
-    }
-
-  } 
+class Player {
+  public void walk() {
+    System.out.println("わーい");
+  }
 }
