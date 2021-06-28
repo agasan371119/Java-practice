62aa90a323e140f4a6eb5a049e25e2134b0669fe
agasan371119 <yna77351unichi@icloud.com>
2021-06-28 14:50:49 +0900

    練習5

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] num = new int[3];
+        int[] num = new int[4];
 
         for(int i = 0; i < 3; i++) {
           int[i] = sc.nextInt();
         }
         Arrays.sort(num);
 
-        System.out.println(num[0] + " " + num[1] + " " + num[2]);
+        System.out.println(num[0] + " " + num[1] + " " + num[2] + " " + num[3]);
     }
 }
