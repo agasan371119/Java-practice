a48c41e879f9a8811552c4037591ed4fb8e132fe
agasan371119 <yna77351unichi@icloud.com>
2021-07-04 18:50:47 +0900

    復習6

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ class Main {
     for(int i = 0; i < count.length; i++) {
       num [i]= sc.nextInt();
       System.out.println(num[i]);
-      System.out.println(" ");      
+      System.out.println(" bye" + " ");      
     }
   }
 }
