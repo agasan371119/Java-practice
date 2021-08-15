cc2693898680bcbe8df6716b38bfd1deeb8117f4
agasan371119 <yna77351unichi@icloud.com>
2021-08-15 10:36:04 +0900

    練習40

--- Main.java
+++ Main.java
@@ -4,12 +4,9 @@ class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     
-    int b = sc.nextInt();
-    int a = sc.nextInt();
-    int sum = a + b;
+    int a = sc.next().length();
 
-
-    System.out.println(sum);
+    System.out.println(a);
 
   }
 }
