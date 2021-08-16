27a8047d4d1972a3d64dcffc6a54310f81477a51
agasan371119 <yna77351unichi@icloud.com>
2021-08-16 19:27:22 +0900

    練習1

--- Main.java
+++ Main.java
@@ -4,9 +4,13 @@ class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     
-    int a = sc.next().length();
-
-    System.out.println(a);
-
+    int ans = 1;
+    for(int i = 0; i < 5; i++) {
+      int num = sc.nextInt();
+      if(num > ans) {
+        ans = num;
+      }
+    }
+    System.out.println(ans);
   }
 }
