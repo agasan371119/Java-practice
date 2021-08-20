1c1ccfd1ad38380c01116dbaf4da9f11b50a30fa
agasan371119 <yna77351unichi@icloud.com>
2021-08-20 19:26:42 +0900

    練習7

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
     Scanner sc = new Scanner(System.in);
     
     int an = 1;
-    for(int i = 0; i < 20; i++) {
+    for(int i = 0; i < 50; i++) {
       int n = sc.nextInt();
       if(n < an) {
         an = n;
