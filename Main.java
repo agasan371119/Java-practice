00d9a3519c961489c9dd6129f2fadd874fcc55e4
agasan371119 <yna77351unichi@icloud.com>
2021-09-11 00:24:10 +0900

    練習14

--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
-class Main {
-  public static void main(String[] args) {
-    
-    int hit = (int)(Math.random() * 10 + 1);
+import java.util.*;
 
-    if(hit < 6) {
-      System.out.println("モンスターは、" + hit + "のダメージを受けた");
-    } else {
-      System.out.println("モンスターにクリティカルヒットのダメージを与えた！！");
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        
+        //空の配列を作成
+        int[] d = new int[c];
+        int[] b = new int[c];
+        //空の配列に入力
+        //数値 a b
+        for(int i = 0; i < c; i++) {
+            d[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+        
+        
+        for(int i = 0; i < c; i++) {
+            System.out.println(d[i] + " " + b[i]);
+        }
     }
-    
-    int crt = (int) (Math.random() * 2 + 1);
-
-    if(crt == 1) {
-      System.out.println("モンスターを倒した！！");
-    } else {
-      System.out.println("モンスターに返り討ちにされた 残念");
-    }
-
-  }
 }
