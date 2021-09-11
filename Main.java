7e7db88904cbc9e3a3d1b831335da328d897d772
agasan371119 <yna77351unichi@icloud.com>
2021-09-11 23:09:25 +0900

    練習1

--- Main.java
+++ Main.java
@@ -4,21 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int c = sc.nextInt();
         
-        //空の配列を作成
-        int[] d = new int[c];
-        int[] b = new int[c];
-        //空の配列に入力
-        //数値 a b
-        for(int i = 0; i < c; i++) {
-            d[i] = sc.nextInt();
-            b[i] = sc.nextInt();
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        
+        for(int i = 0 ; i < n; i++) {
+            num[i] = sc.nextInt();
         }
         
-        
-        for(int i = 0; i < c; i++) {
-            System.out.println(d[i] + " " + b[i]);
+        for(int s : num) {
+            if(s == 0) {
+                System.out.println("NO");
+                sc.close();
+                return;
+            }
         }
+        System.out.println("YES");
+        sc.close();
     }
 }
