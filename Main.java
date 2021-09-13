7e2faba1ecdc9011018101b065ee355dc3985f01
agasan371119 <yna77351unichi@icloud.com>
2021-09-13 13:51:23 +0900

    練習13

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int n = sc.nextInt();
+        int nu = sc.nextInt();
         int[] num = new int[n];
         
-        for(int i = 0 ; i < n; i++) {
+        for(int i = 0 ; i < nu; i++) {
             num[i] = sc.nextInt();
         }
         
