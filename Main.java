774922f7e7087713bd5077ee83118347e4a33417
agasan371119 <yna77351unichi@icloud.com>
2021-09-20 21:12:29 +0900

    練習20

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        String st = sc.next();
         
         System.out.println(n);
+        System.out.println(st);
     }
 }
