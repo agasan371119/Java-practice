3784cb2f4bbf6347cdd3c7925505dd4e14643c62
agasan371119 <yna77351unichi@icloud.com>
2021-10-08 11:07:02 +0900

    練習37

--- Main.java
+++ Main.java
@@ -5,15 +5,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
-        if(i % 3 == 0 && i % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if(i % 3 == 0) {
-            System.out.println("Fizz");
-        } else if(i % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(i);
+
+        for(int i = 0; i < n; i++) {
+            //13:00 時刻
+            String statr = sc.next();
+            //1時間
+            int h = sc.nextInt();
+            //30分
+            int m = sc.nextInt();
+            
+            //時刻(文字列)を数値に変換
+            int startH = Integer.parseInt(start.substring(0, 2));
+            int startm = Integer.parseInt(start.substring(3, 5));
+            
+            int ah = startH + h;
+            int am = startM + m;
+
+            if(am >= 60) {
+                ah += 1;
+                am -= 60;
+            }
+
+            if(ah > 24) {
+                ah -= 24;
+            }
+
+            String endH = Integer.toString(ah);
+            String endM = Integer.toString(am);
+
+            if(endH.length() == 1) {
+                endH = "0" + endH;
+            }
+
+            if(endM.length() == 1) {
+                endM = "0" + endM;
+            }
+            System.out.println(endH + ":" + endM);
+
         }
     }
 }
