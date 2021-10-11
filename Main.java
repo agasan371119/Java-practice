5055d3936b58b35603f6926e3c0f6d24fc5f2118
agasan371119 <yna77351unichi@icloud.com>
2021-10-11 15:21:13 +0900

    練習40

--- Main.java
+++ Main.java
@@ -4,46 +4,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        
+        int count = sc.nextInt();
+        
+        int[] numArray = new int[count];
+        
+        for(int i = 0; i < count; i++) {
+            numArray[i] = sc.nextInt(); 
+        }
+        
+        int k = sc.nextInt();
 
-        for(int i = 0; i < n; i++) {
-            //13:00 時刻
-            String statr = sc.next();
-            //1時間
-            int h = sc.nextInt();
-            //30分
-            int m = sc.nextInt();
-            
-            //時刻(文字列)を数値に変換
-            int startH = Integer.parseInt(start.substring(0, 2));
-            int startm = Integer.parseInt(start.substring(3, 5));
-            
-            //数値に時間と分を加える
-            int ah = startH + h;
-            int am = startM + m;
-
-            //
-            if(am >= 60) {
-                ah += 1;
-                am -= 60;
+        for(int i = 0; i < count; i++) {
+            if(numArray[i] == k) {
+                System.out.println(i + 1);
+                //breakで終了させる
+                break;
             }
-
-            if(ah > 24) {
-                ah -= 24;
-            }
-
-            String endH = String.valueOf(ah);
-            String endM = String.valueOf(am);
-
-            if(endH.length() == 1) {
-                endH = "0" + endH;
-            }
-
-            if(endM.length() == 1) {
-                endM = "0" + endM;
-            }
-            System.out.println(endH + ":" + endM);
-
         }
     }
-}
+}s
