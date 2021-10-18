897bb07f6e94af3079f796d9d284b87dd4fb2719
agasan371119 <yna77351unichi@icloud.com>
2021-10-18 09:45:45 +0900

    練習36

--- Main.java
+++ Main.java
@@ -3,18 +3,42 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList <String> str = new ArrayList<>();
-
-        str.add("勇者");
-        str.add("戦士");
-        str.add("魔法使い");
-        str.add("僧侶");
-        str.add("賢者");
-        str.add("遊び人");
-        str.add("武道家");
+        Scanner sc = new Scanner(System.in);
         
-        for(int i = 0; i < str.size(); i++) {
-            System.out.println(str.get(i));
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            String str = sc.next();
+            int h = sc.nextInt();
+            int m = sc.nextInt();
+            
+            int h2 = Integer.parseInt(str.substring(0, 2));
+            int m2 = Integer.parseInt(str.substring(3, 5));
+            
+            h2 = h2 + h;
+            m2 = m2 + m;
+            
+            if(m2 >= 60) {
+                h2 += 1;
+                m2 -= 60; 
+            }
+            
+            if(h2 >= 24) {
+                h2 -= 24;
+            }
+            
+            String h3 = Integer.toString(h2);
+            String m3 = Integer.toString(h2);
+            
+            if(h3.length() == 1) {
+                h3 = "0" + h3;
+            }
+
+            if(m3.length() == 1) {
+                m3 = "0" + m3;
+            }
+            
+            System.out.println(h3 + ":" + m3);
         }
     }
 }
