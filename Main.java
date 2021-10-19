63a31732db2baedc8bef52d82e2a1c03ddc96d4f
agasan371119 <yna77351unichi@icloud.com>
2021-10-19 18:18:39 +0900

    練習3

--- Main.java
+++ Main.java
@@ -1,44 +1,17 @@
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int ans = 0;
         
-        int n = sc.nextInt();
-        
-        for(int i = 0; i < n; i++) {
-            String str = sc.next();
-            int h = sc.nextInt();
-            int m = sc.nextInt();
-            
-            int h2 = Integer.parseInt(str.substring(0, 2));
-            int m2 = Integer.parseInt(str.substring(3, 5));
-            
-            h2 = h2 + h;
-            m2 = m2 + m;
-            
-            if(m2 >= 60) {
-                h2 += 1;
-                m2 -= 60; 
-            }
-            
-            if(h2 >= 24) {
-                h2 -= 24;
-            }
-            
-            String h3 = Integer.toString(h2);
-            String m3 = Integer.toString(h2);
-            
-            if(h3.length() == 1) {
-                h3 = "0" + h3;
-            }
+        for(int i = 0; i < 3; i++) {
+            int n = sc.next().length();
 
-            if(m3.length() == 1) {
-                m3 = "0" + m3;
+            if(a < n) {
+                a = n;
             }
-            
-            System.out.println(h3 + ":" + m3);
         }
+        System.out.println(a);
     }
 }
