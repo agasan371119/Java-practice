10b948835efe542ce12832afcd85fd46636f25af
agasan371119 <yna77351unichi@icloud.com>
2021-10-21 00:02:03 +0900

    練習5

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int ans = 0;
         
-        for(int i = 0; i < 3; i++) {
+        for(int i = 0; i < n; i++) {
             String b = sc.next();
             int n = b.length();
-            
+
             if(a < n) {
                 a = n;
             }
