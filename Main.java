16dad23c12ebbcb3ded92bf002cb3aa05ec68721
agasan371119 <yna77351unichi@icloud.com>
2021-10-22 21:35:32 +0900

    練習6

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int ans = 0;
         
-        for(int i = 0; i < n; i++) {
-            String b = sc.next();
-            int n = b.length();
-
-            if(a < n) {
-                a = n;
-            }
+        int sum = 0;
+        int i = n;
+        while(i < 10) {
+            System.out.println(i);
+            sum++;
+            i += 1;
+        } 
+        int m = 10 - sum;
+        for(int j = 0; j < m; j++) {
+            System.out.println(j);
         }
-        System.out.println(a);
     }
 }
