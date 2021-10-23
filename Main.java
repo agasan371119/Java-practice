4df98b47227964ae81c5fae2cc1984483f9e32ce
agasan371119 <yna77351unichi@icloud.com>
2021-10-23 15:15:21 +0900

    練習7

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         while(i < 10) {
             System.out.println(i);
             sum++;
-            i += 1;
+            i = i + 1;
         } 
         int m = 10 - sum;
         for(int j = 0; j < m; j++) {
