2afabe49799220ed556e5d44834bdc54c0844810
agasan371119 <yna77351unichi@icloud.com>
2021-11-08 18:11:06 +0900

    練習22

--- Main.java
+++ Main.java
@@ -4,18 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        for(int i = 1; i <= n; i++) {
-            //System.out.println(i);
-            for(int j = 1; j <= i; j++) {
-                System.out.print(j);
-                if(j < i ) {
-                    System.out.print(" ");
-                } else {
-                    System.out.println("||---- | ----- || ----- | ----- || - | - || - | - || - | - ||");
-                }
-            }
-        }
+
+        double x = sc.nextDouble();
+        int n = (int)(Math.round(x));
+        System.out.println(n);
     }
 } 
