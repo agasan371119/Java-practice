86b9dce68f64c496e9ca77fb2321b3e5b6441f73
agasan371119 <yna77351unichi@icloud.com>
2021-11-09 16:23:51 +0900

    sortの練習

--- Main.java
+++ Main.java
@@ -3,10 +3,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        double x = sc.nextDouble();
-        int n = (int)(Math.round(x));
-        System.out.println(n);
+        String[] n = {"1121", "0112", "0103", "1111"};
+
+        Arrays.sort(n);
+
+        for(int i = 0; i < 4; i++) {
+            System.out.println(n[i]);
+        }
     }
 } 
