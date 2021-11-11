a496944051c6dfce0a88b4cb6884559f57612ddb
agasan371119 <yna77351unichi@icloud.com>
2021-11-11 14:58:04 +0900

    練習3

--- Main.java
+++ Main.java
@@ -5,24 +5,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        //空のHashMap作成
         HashMap<String, Integer> point = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
+        //HashMapに格納
+        for(int i = 0; i < n; i++) {
             String s = sc.next();
-            
             point.put(s, 0);
         }
 
         int m = sc.nextInt();
+        for(int i = 0; i < m; i++) {
+            String t = sc.next();
+            int l = sc.nextInt();
 
-        for (int i = 0; i < m; i++) {
-            String p = sc.next();
-            int a = sc.nextInt();
-            
-            point.put(p, point.get(p) + a);
+            point.put(t, point.get(t) + l);
         }
-
-        System.out.println(point.get(sc.next()));
+        String r = sc.next();
+        System.out.println(point.get(r));
     }
 }
