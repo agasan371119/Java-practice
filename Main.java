18b999c5223e5b3f3a36ea527ca1e3dcb4478999
agasan371119 <yna77351unichi@icloud.com>
2021-11-13 17:03:52 +0900

    ArryaList練習1

--- Main.java
+++ Main.java
@@ -3,12 +3,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] name = {"b", "a", "c", "d"};
+        ArrayList<String> list = new ArrayList<>();
+        list.add("b");
+        list.add("a");
+        list.add("e");
+        list.add("g");
 
-        Arrays.sort(name);
-        for(int i = 0; i < 4; i++) {
-            System.out.println(name[i]);
+        Collections.sort(list);
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
-
     }
 }
