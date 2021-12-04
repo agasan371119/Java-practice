fee77fe322a11c0315765f50e469a8fb7aec5152
agasan371119 <yna77351unichi@icloud.com>
2021-12-04 10:28:55 +0900

    練習2

--- Main.java
+++ Main.java
@@ -3,36 +3,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
-        list.add("b");
-        list.add("a");
-        list.add("e");
-        list.add("g");
-        list.add("z");
-        list.add("A");
-        list.add("B");
-        list.add("1");
-        list.add("3");
-        list.add("5");
-        list.add("10");
-        list.add("z");
-        list.add("あ");
-        list.add("か");
-        list.add("ぃ");
-        list.add("お");
-        list.add("13");
-        list.add("16");
-        list.add("21");
-        list.add("ab");
-        list.add("cd");
-        list.add("ef");
-        list.add("eg");
-        list.add("er");
-        list.add("ez");
 
-        Collections.sort(list);
-        for(int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        int n = sc.nextInt();
+
+        if(n >= 42) {
+            n = n + 1;
+        } else {
+            n = n;
         }
+
+        String n1 = Integer.toString(n);
+
+        while(n1.length() < 3) {
+            n1 = "0" + n1;
+        }
+
+        System.out.println("AGC"  + n1);
     }
 }
