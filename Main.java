46b55903507f4d253c79e74bfbe5f89456c69e39
agasan371119 <yna77351unichi@icloud.com>
2022-03-24 01:47:18 +0900

    練習109

--- Main.java
+++ Main.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        //カウント
         int n = sc.nextInt();
+
+        //空の配列作成
         int[] m = new int[n];
 
+        //配列に入力
         for(int i = 0; i < n; i++) {
             m[i] = sc.nextInt();
         }
-
+        //配列から出力
         for(int i = 0; i < n; i++) {
             System.out.println(m[i]);
         }
