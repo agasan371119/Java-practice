a54c489f62d91a111720d2163060105f15ccdc5e
agasan371119 <yna77351unichi@icloud.com>
2022-04-05 12:42:35 +0900

    練習119

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //カウント
-        int n = 45;
+        int n = 460
 
         //空の配列作成
         int[] m = new int[n];
