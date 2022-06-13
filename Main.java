2d348626dbb356dac5b31b8c0283f04aa1577d57
agasan371119 <yna77351unichi@icloud.com>
2022-06-13 21:04:55 +0900

    練習127

--- Main.java
+++ Main.java
@@ -3,21 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int[] a = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-
-        int k = sc.nextInt();
-
-        int sum = 0;
-
-        for(int i = n - 1; i > -1 ; i-- ) {
-            System.out.println(i);
-        }
-
+        System.out.println(Math.max(10, 11));
+        System.out.println(Math.min(10, 11));
     }
 }
