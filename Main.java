996fceed8e5593352023d7bcdaad158992cd5567
agasan371119 <yna77351unichi@icloud.com>
2022-08-29 20:06:51 +0900

    練習128

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.util.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(Math.max(10, 11));
-        System.out.println(Math.min(10, 11));
+        int n = sc.nextInt();
+        System.out.println(n);
     }
 }
