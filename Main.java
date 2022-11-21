12981d4ceccfeaeb0d048dcddbed22b3f822596b
agasan371119 <yna77351unichi@icloud.com>
2022-11-21 21:41:36 +0900

    復習7

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
             m[i] = sc.nextInt();
         }
         for(int l : m) {
-            System.out.println(m[i] - k);
+            System.out.println(m[i] - k + k);
         }
     }
 }
