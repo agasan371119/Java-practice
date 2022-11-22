f821b5112b85a9c4746367321aa28fc7d03c95ff
agasan371119 <yna77351unichi@icloud.com>
2022-11-22 20:59:26 +0900

    復習8

--- Main.java
+++ Main.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int ;l = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] m = new int[n];
+        int[] n = new int[n];
 
         for(int i = 0; i < n; i++) {
-            m[i] = sc.nextInt();
+            n[i] = sc.nextInt();
         }
         for(int l : m) {
-            System.out.println(m[i]);
+            System.out.println(n[i]);
         }
     }
 }
