e1732a8107dd0f62e46f5e5478791130b5036037
agasan371119 <yna77351unichi@icloud.com>
2022-11-23 16:09:06 +0900

    復習9

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ;l = sc.nextInt();
+        int l = sc.nextInt();
         int k = sc.nextInt();
 
         int[] n = new int[n];
