f6d96a3e077b4116c8478ff904a63438d38fcb9e
agasan371119 <yna77351unichi@icloud.com>
2022-11-27 19:18:23 +0900

    復習13

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
         } else if(s < 9) {
             System.out.println("残念");
         } else {
-            System.out.println("無念です");
+            System.out.println("無念");
         }
     }
 }
