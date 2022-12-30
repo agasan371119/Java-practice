21dd97d533f246303a04a1730f4e212abfac6e7a
agasan371119 <yna77351unichi@icloud.com>
2022-12-31 06:52:00 +0900

    復習25

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         int s = sc.nextInt();
 
         if(s > 10) {
-            System.out.println("やった!");
+            System.out.println("やってる!");
         } else if(s < 9) {
             System.out.println("わかった!!!");
         } else {
